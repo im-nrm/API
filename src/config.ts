cb3ace60ff1cb10c9b4d403bd4cec22c9fb258eb
im-nrm <aitorfernandezsm@gmail.com>
2024-06-09 21:22:57 +0200

    API v0

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-const DB_URL = 'mongodb://localhost:27017/';
+// const DB_URL = 'mongodb://localhost:27017/';
+const DB_URL = 'mongodb+srv://nrm:api_test@api.33sl9ib.mongodb.net/?retryWrites=true&w=majority&appName=API'
 
 const dbconnect = () =>{
     mongoose.set('strictQuery', true)
-    mongoose.connect(DB_URL+'API_test', {}).then(
+    mongoose.connect(DB_URL+'/sample_mflix', {}).then(
         () => { console.log('Connexion to DB OK') },
         (err: Error) => { console.log('Connexion to DB FAILED', err) }
       );
